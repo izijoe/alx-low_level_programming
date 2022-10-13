c7e8913294136cf1fe451fe950781610343ef8b8
izijoe <jospaco2001@yahoo.com>
2022-10-13 15:23:24 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
