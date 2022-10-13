2f2e61d2c6b920583c7abbc022e7f977ea3e7e2a
izijoe <jospaco2001@yahoo.com>
2022-10-13 14:43:42 -0700

    task 4 updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - Entry Point
  *
  * Return: Always 0 (success)
- 
  */
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
-
 }
