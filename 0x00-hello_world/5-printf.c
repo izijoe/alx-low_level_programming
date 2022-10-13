b052058d87f793d2fb4c3352576fc62855088239
izijoe <jospaco2001@yahoo.com>
2022-10-13 13:18:56 -0700

    5-printf.c program that prints exactly

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints a texting
+ *
+ * Return: Always 0 (Successful)
+ */
+init main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
