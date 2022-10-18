13b4ff01ecf590eb9a2a711d0477cbf3f22dbb37
izijoe <jospaco2001@yahoo.com>
2022-10-18 13:42:53 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ * is writen by izijoe
+ * Return: 0
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchat('r');
+_putchar('\n');
+return (0);
+}
