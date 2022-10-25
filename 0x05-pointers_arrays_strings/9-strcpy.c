bfa4ee0269cdacd9a242258f9648a1641fc04fa6
izijoe <jospaco2001@yahoo.com>
2022-10-24 17:09:47 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,10 +12,11 @@ char *_strcpy(char *dest, char *src)
 
 	while (*(src + len) != '\0')
 	{
-		(*dest + len)  * (src + len);
+		*(dest + len) = *(src + len);
 		len++;
+
 	}
 
-	(*dest + len)  '\0';
+	*(dest + len) = '\0';
 	return (dest);
 }
