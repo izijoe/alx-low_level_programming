b480bb2614e19028b99ffeec253b4bdcf3d160d0
izijoe <jospaco2001@yahoo.com>
2022-11-04 11:59:36 -0700

    main.h

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - writes the character cto stdout
+ * @c: The character to print
+ * Return: on success 1.
+ * on error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+
+#endif
