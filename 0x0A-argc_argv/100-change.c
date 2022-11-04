c1946fd4bf4822d26f24083497b74a35177a8736
izijoe <jospaco2001@yahoo.com>
2022-11-04 16:57:50 -0700

    task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 
 	/* convert string to int andcalculate coins */
 	n = atoi(argv[1]);
-	coins += n / 25
+	coins += n / 25;
 	n = n % 25;
 	coins += n / 10;
 	n = n % 10;
