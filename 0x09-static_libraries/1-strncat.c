426510b14374faa4791d3266e9fd172c6fe676ca
izijoe <jospaco2001@yahoo.com>
2022-11-05 15:12:40 -0700

    task 1

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings ussing at most
+ * aan inputted number of bytes from src
+ * @dest: the strings to be appended to upon
+ * @src: the strings to be appended to dest
+ * @n: the number of bytes from src to be appended to dest
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0, src[index] && index < n; index++;)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
