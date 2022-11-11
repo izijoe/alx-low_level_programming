4ec423f6eb0e83a5eb75f2c648e2439ff12d5ebe
izijoe <jospaco2001@yahoo.com>
2022-11-11 13:28:45 -0800

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,35 +14,34 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *sout;
-	unsigned int ls1, ls2, lsout, i,
-		     
-		     	
-		     	if (s1 == NULL)
-				
-				s1 = "";
+	unsigned int ls1, ls2, lsout, i;
 
-			if (s2 == NULL)
-				
-				s2 = "";
+	if (s1 == NULL)
 
-			for (ls1 = 0; s1[ls1] != '\0'; ls1++)
-			     ;
-			for (ls2 = 0; s2[ls2] != '\0'; ls2++)
-			     ;
-			if (n > ls2)
-			     n = ls2;
-		     lsout = ls1 + n;
-		     sout = malloc (lsout + 1);
+		s1 = "";
 
-		     if (sout == NULL)
-			     return (NULL);
+	if (s2 == NULL)
 
-		     if (i = 0; i < lsout; i++)
-			     if (i < ls1)
-				     sout[i] = s1[i];
-		     	     else
-				     sout[i] = s2[1 - ls1];
-		     sout[i] = '\0';
-		     
-		     return (sout);
+		s2 = "";
+
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
+	if (n > ls2)
+		n = ls2
+	lsout = ls1 + n;
+	sout = malloc(lsout + 1);
+
+	if (sout == NULL)
+		;
+	return (NULL);
+
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
+	sout[i] = '\0';
+	return (sout);
 }
