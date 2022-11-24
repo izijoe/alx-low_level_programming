e2d60fc1da29b8b369c92f06a406b6960524b6e1
izijoe <jospaco2001@yahoo.com>
2022-11-24 15:56:33 -0800

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * first - function executed before main
+ * Return: no return.
+ */
+void _attribute_((constructor)) first()
+{
+	printf("you're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
