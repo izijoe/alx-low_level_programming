2e31699740c7a949fb89f7179705636bcee325ea
izijoe <jospaco2001@yahoo.com>
2023-01-07 13:54:03 -0800

    2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,24 +1,28 @@
 #include "main.h"
 
 /**
- * _strncpy - copies at most an inputted number
- * of bytes from string src to dest
- * @dest: buffer storing the string copy
- * @src: the source string
- * @n: the maximum number of byte to be copied from src
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
  *
- * Return: a pointer to  the resulting dest
+ * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0, src_len = 0;
+	int j;
 
-	while (src[index++])
-		src_len++;
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
 
-	for (index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
-	for (index = src_len; index < n; index++)
-		dest[index] = '\0';
 	return (dest);
 }
