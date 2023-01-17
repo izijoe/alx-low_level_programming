d7ff5cd2db55709c98484ccb68968eff6b8d9938
izijoe <jospaco2001@yahoo.com>
2023-01-17 06:21:11 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -0 /tmp/win.so
-https://github.com/ 
+wget -o /tmp/win.so https://github.com/izijoe/alx-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
